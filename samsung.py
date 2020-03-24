62426495cabb4d8cd02f76d408148ce370bbe49f
Esther Cecilia <estherazapfy@gmail.com>
2020-03-24 09:33:40 -0300

    A

--- samsung.py
+++ samsung.py
@@ -175,6 +175,7 @@ class Sala(Resource):
         sala = Salas.query.filter_by(nome=nome).first()
         try:
             response = {
+                'status': True
                 'nome' : sala.nome,
                 'senha' : sala.senha,
                 'id' : sala.id
